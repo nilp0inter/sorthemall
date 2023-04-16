b1d418bff4c2f9c85e59440a0a773e30282cac5f
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-04-16 20:19:27 +0200

    feature: elo decay

--- sorthemall.py
+++ sorthemall.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 import argparse
 import sys
 
@@ -5,8 +6,8 @@ from gptrun import chatgptrun
 from tinydb import TinyDB, Query
 
 # Constants
-DEFAULT_ELO = 1200
-K_FACTOR = 32
+DEFAULT_ELO = 1500
+K_FACTOR = 40
 
 
 def create_parser():
@@ -35,27 +36,31 @@ def create_parser():
     return parser
 
 
-def update_elo_score(score1, score2, outcome):
+def update_elo_score(k_factor, score1, score2, outcome):
     rating_diff = score2 - score1
     expected_outcome1 = 1 / (1 + 10 ** (rating_diff / 400))
     expected_outcome2 = 1 - expected_outcome1
 
     if outcome == "1":
-        new_score1 = score1 + K_FACTOR * (1 - expected_outcome1)
-        new_score2 = score2 + K_FACTOR * (0 - expected_outcome2)
+        new_score1 = score1 + k_factor * (1 - expected_outcome1)
+        new_score2 = score2 + k_factor * (0 - expected_outcome2)
     elif outcome == "2":
-        new_score1 = score1 + K_FACTOR * (0 - expected_outcome1)
-        new_score2 = score2 + K_FACTOR * (1 - expected_outcome2)
+        new_score1 = score1 + k_factor * (0 - expected_outcome1)
+        new_score2 = score2 + k_factor * (1 - expected_outcome2)
     else:
-        new_score1 = score1 + K_FACTOR * (0.5 - expected_outcome1)
-        new_score2 = score2 + K_FACTOR * (0.5 - expected_outcome2)
+        new_score1 = score1 + k_factor * (0.5 - expected_outcome1)
+        new_score2 = score2 + k_factor * (0.5 - expected_outcome2)
 
     return new_score1, new_score2
 
 
+@lru_cache
 @chatgptrun(
     api_temperature=0,
-    # on_invalid_response=lambda: None
+    on_invalid_response=lambda: None,
+    api_frequency_penalty=-2,
+    api_presence_penalty=-2,
+    api_max_tokens=10
 )
 def cmp_object_for_task(task, obj1, obj2):
     """
@@ -70,6 +75,8 @@ def cmp_object_for_task(task, obj1, obj2):
     None
     >>> cmp_object_for_task("magnify", "ペン", "glasses")
     "glasses"
+    >>> cmp_object_for_task("писать", "knife", "ペン")
+    "ペン"
     """
     ...
 
@@ -101,7 +108,7 @@ def main():
             print("Invalid winner. Please choose either 1, 2, or 0 for tie.")
             sys.exit(1)
 
-        new_score1, new_score2 = update_elo_score(score1, score2, args.winner)
+        new_score1, new_score2 = update_elo_score(K_FACTOR, score1, score2, args.winner)
 
         db.update({args.dimension: new_score1}, Object.object_id == args.object_id1)
         db.update({args.dimension: new_score2}, Object.object_id == args.object_id2)
@@ -114,33 +121,36 @@ def main():
 
         all_objects = db.all()
 
-        for i in range(len(all_objects)):
-            for j in range(i + 1, len(all_objects)):
-                obj1 = all_objects[i]
-                obj2 = all_objects[j]
+        for k_decay in range(K_FACTOR // 2):
+            for i in range(len(all_objects)):
+                for j in range(i + 1, len(all_objects)):
+                    obj1 = all_objects[i]
+                    obj2 = all_objects[j]
 
-                oid1 = obj1["object_id"]
-                oid2 = obj2["object_id"]
-                task = dimension
-                outcome = cmp_object_for_task(task, oid1, oid2)
-                print(f'{task=} {oid1=} {oid2=} {outcome=}')
+                    oid1 = obj1["object_id"]
+                    oid2 = obj2["object_id"]
+                    task = dimension
+                    outcome = cmp_object_for_task(task, oid1, oid2)
 
-                if outcome == oid1:
-                    result = "1"
-                elif outcome == oid2:
-                    result = "2"
-                elif outcome is None:
-                    result = "0"
-                else:
-                    raise ValueError("Unknown result {outcome!r}")
+                    if k_decay == 0:  # Only print in the first round
+                        print(f'{task=} {oid1=} {oid2=} {outcome=}')
 
-                score1 = obj1.get(dimension, DEFAULT_ELO)
-                score2 = obj2.get(dimension, DEFAULT_ELO)
+                    if outcome == oid1:
+                        result = "1"
+                    elif outcome == oid2:
+                        result = "2"
+                    elif outcome is None:
+                        result = "0"
+                    else:
+                        raise ValueError("Unknown result {outcome!r}")
 
-                new_score1, new_score2 = update_elo_score(score1, score2, result)
+                    score1 = obj1.get(dimension, DEFAULT_ELO)
+                    score2 = obj2.get(dimension, DEFAULT_ELO)
 
-                db.update({dimension: new_score1}, Object.object_id == obj1["object_id"])
-                db.update({dimension: new_score2}, Object.object_id == obj2["object_id"])
+                    new_score1, new_score2 = update_elo_score(K_FACTOR - k_decay, score1, score2, result)
+
+                    db.update({dimension: new_score1}, Object.object_id == obj1["object_id"])
+                    db.update({dimension: new_score2}, Object.object_id == obj2["object_id"])
 
         print("Automatically updated ELO scores for all objects and dimensions.")
     elif args.command == "show":
@@ -151,7 +161,7 @@ def main():
 
         print(f"Sorted objects by dimension '{dimension}':")
         for obj in sorted_objects:
-            print(f"Object ID: {obj['object_id']}, {dimension}: {obj.get(dimension, DEFAULT_ELO)}")
+            print(f"{obj['object_id']}: {obj.get(dimension, DEFAULT_ELO)}")
 
 
 
