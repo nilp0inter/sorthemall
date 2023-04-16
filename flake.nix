4be752dbdbd25453b05342b414eedb01d5b8d2d0
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-04-16 18:45:16 +0200

    fix: problems with glibc

--- flake.nix
+++ flake.nix
@@ -10,10 +10,10 @@
         pkgs.zlib
       ];
 
-      shellHook = ''
-        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
-        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
-      '';
+      # shellHook = ''
+      #   export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+      #   export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+      # '';
 
     };
   };
