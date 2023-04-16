f95304c445609b9e1d9e64ffd59845c0720667ff
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2023-04-16 18:42:14 +0200

    chore: nix env

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  outputs = { self, nixpkgs }: let
+    pkgs = nixpkgs.legacyPackages.x86_64-linux;
+  in
+  {
+    devShells.x86_64-linux.default = pkgs.mkShell rec {
+
+      buildInputs = [
+        pkgs.pipenv
+        pkgs.zlib
+      ];
+
+      shellHook = ''
+        export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath buildInputs}:$LD_LIBRARY_PATH"
+        export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib.outPath}/lib:$LD_LIBRARY_PATH"
+      '';
+
+    };
+  };
+}
